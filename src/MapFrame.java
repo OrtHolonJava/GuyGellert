0b499f2eb272143c4328ebcb08b3ca8bf013ef56
גיא גלרט < @GG-PC>
2017-12-16 17:06:50 +0200

    working on Moutain map

--- src/MapFrame.java
+++ src/MapFrame.java
@@ -4,13 +4,16 @@ public class MapFrame extends JFrame {
 
 	private MapPanel _mp;
 	private MapSnowPanel _msp;
+	private MapMoutainsPanel _mmp;
 	public MapFrame()
 	{
 		super();
 		_mp = new MapPanel();
 		_msp = new MapSnowPanel();
+		_mmp = new MapMoutainsPanel();
 		//add(_mp);
-		add(_msp);
+		//add(_msp);
+		add(_mmp);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(900, 700);
 		setVisible(true);
