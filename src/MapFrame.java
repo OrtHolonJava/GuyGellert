67507df2cb1694a99384265bca1abb04d991e8b9
גיא גלרט < @GG-PC>
2017-12-26 08:49:12 +0200

    working on City map

--- src/MapFrame.java
+++ src/MapFrame.java
@@ -5,15 +5,18 @@ public class MapFrame extends JFrame {
 	private MapPanel _mp;
 	private MapSnowPanel _msp;
 	private MapMoutainsPanel _mmp;
+	private MapCityPanel _mcp;
 	public MapFrame()
 	{
 		super();
 		_mp = new MapPanel();
 		_msp = new MapSnowPanel();
 		_mmp = new MapMoutainsPanel();
+		_mcp = new MapCityPanel();
 		//add(_mp);
 		//add(_msp);
-		add(_mmp);
+		//add(_mmp);
+		add(_mcp);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(900, 700);
 		setVisible(true);
